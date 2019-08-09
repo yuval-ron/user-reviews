959878e1943bc93bb5acb62be5338b7f1f8129f5
Yuval Ron <yuvaly2394@gmail.com>
2019-08-09 15:22:43 +0300

    Add design for review item

--- src/reviews/components/ReviewItem.js
+++ src/reviews/components/ReviewItem.js
@@ -2,13 +2,28 @@ import React, {Component} from 'react'
 
 import '../styles/ReviewItem.css'
 
-export default class Reviews extends Component {
+const imgGen = require('@dudadev/random-img')
+
+export default class ReviewItem extends Component {
+  state = {
+    avatarURL: ''
+  }
+
+  componentDidMount() {
+    imgGen().then(avatarURL => {
+      console.log('ReviewItem.js avatarURL', avatarURL)
+      this.setState({avatarURL})
+    });
+  }
+
   render() {
     const {review: {name, comment}} = this.props
+    const {avatarURL} = this.state
+    const avatarStyle = {backgroundImage: `url(${avatarURL})`}
 
     return (
       <div className="review-item-container">
-        <div className="user-avatar"></div>
+        <div className="user-avatar" style={avatarStyle}></div>
         <div className="review-details">
           <div className="name">{name}</div>
           <div className="comment">{comment}</div>
