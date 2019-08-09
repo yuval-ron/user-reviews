0edafd94fab1425a55d66791fe5dfd2295097be1
Yuval Ron <yuvaly2394@gmail.com>
2019-08-09 14:11:14 +0300

    First commit (clear create-react-app leftovers)

---
+++ src/app/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Navbar from './Navbar'
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
